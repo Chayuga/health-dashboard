b7c9adffd89e48f23c8a968413d0869eb6a31870
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-12-02 15:51:05 +0300

    view: display dashboard to view

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Dashboard from '../components/dashboard/Dashboard';
 export default function Home() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center py-2 bg-gray-200'>
-      Hello world!
+      <Dashboard />
     </div>
   );
 }
