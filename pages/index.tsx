035ad26eede235758bbad85e9b9deb9b94bc1011
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-12-02 15:09:29 +0300

    build: dashboard layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
-import Head from 'next/head';
+import Dashboard from '@/components/dashboard/Dashboard';
 
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
 }
