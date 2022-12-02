b5e03288a9466763145747c6294e595f5d41ce1c
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-12-02 15:29:05 +0300

    fix: tailwind styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2 bg-gray-200'>
+      Hello world!
+    </div>
+  );
 }
