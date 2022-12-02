951d67d7fd89541c7eeeacaedb6b97abdd38c7d0
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-12-02 13:25:47 +0300

    chore: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      md: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+};
